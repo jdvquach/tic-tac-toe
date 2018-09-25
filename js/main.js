6129ce2a20de0aea55ed561cb77a6b4b3a60a066
John Quach <John.DV.Quach@gmail.com>
2018-09-25 21:52:50 +1000

    Added logic for checking winnings

--- js/main.js
+++ js/main.js
@@ -1,121 +1,122 @@
-let player1Name="" , player2Name="", turn = "";
-// let board =  [[0,0,0],[0,0,0],[0,0,0]];
-let board =  [0,0,0,0,0,0,0,0,0];
+// WDI29 - Tic Tac Toe
+//
+//
+
+//Set global variables
+let player1Name = "" , player2Name = "", turn = "";
+let board = [0,0,0,0,0,0,0,0,0];
 let winnerFound = 0, moveCount=0;
 
+//Function to broadcast message
 const boardMsg = function (x){
 	return $("#board").text(x);
 }
 
+//Function to randomly choose player 1 or player 2
 const setTurn =function (){
-	let r = Math.floor((Math.random() * 2) + 1);
+	let randomPlayer = Math.floor((Math.random() * 2) + 1);
 	winnerFound=0;
-	if(r==1){
+	if ( randomPlayer == 1 ){
 		turn = player1Name;
-		boardMsg(player1Name+"'s turn now!");
+		boardMsg( player1Name+"'s turn now!" );
 	}
-	else{
+	else {
 		turn = player2Name;
-		boardMsg(player2Name+"'s turn now!");
+		boardMsg( player2Name+"'s turn now!" );
 	}
 }
-
-const init = function (){
+//Initialise at start and on win/draw
+const init = function () {
 		turn = "";
-		// board =  [[0,0,0],[0,0,0],[0,0,0]];
-    board =  [0,0,0,0,0,0,0,0,0];
+    board = [0,0,0,0,0,0,0,0,0];
 		boardMsg("");
-		// $(".col").map(function() {
-    // 		$(this).text("");
-		// }).get();
 		winnerFound = 0;
 		moveCount=0;
 }
 
-$("#playButton").click(function (){
+$("#playButton").click(function () {
 
-	if(winnerFound==1){
+	if ( winnerFound == 1 ) {
 		init();
 	}
 
 	player1Name = $("#player1").val();
 	player2Name = $("#player2").val();
 
-	if(player1Name=="" || player2Name==""){
-		alert("Please set player all the names.");
+	if (player1Name == "" || player2Name == ""){
+		alert( "Please set all player names." );
 		return;
 	}
-
 	setTurn();
 
 });
 
 $(".col").click(function (){
 
-  let boxIndex = alert( $(".col").index(this) +1 );  //Just want to know what the index is for the box clicked.
-  console.log(boxIndex);    //Why  not showing in console log? Undefined
-	if(player1Name=="" || player2Name==""){
-		alert("Please set player all the names.");
+	const id = parseInt( $(this).attr('id') );
+	console.log(id);
+
+	board[id] = turn;
+
+	if(player1Name == "" || player2Name == ""){
+		alert("Please set all player names.");
+		return;
+	}
+  console.log(board[id]);
+	// if (board[id]!== 0) {
+	// 	alert("This position is taken. Please try other position.");
+	// 	return;
+	// }
+
+	if ( winnerFound == 1 ) {
+		alert( "Please click play again" );
 		return;
 	}
 
-	let row = $(this).parent().index();
-
-	let col = $(this).index();
-
-	console.log(this.row,this.col);
-
-	if(board[row][col]!==0){
-		alert("This position is taken. Please try other position.");
-		return;
-	}
-	if(winnerFound==1){
-		alert("Please click play again");
-		return;
-	}
-
-	if(turn==player1Name){
+	if ( turn == player1Name ) {
 		moveCount++;
 		$(this).text("O");
-		board[row][col] = 1;
-		let ifWon = winnerCheck(1,player1Name);
-		if(!ifWon){
-			if(moveCount>=9){
+
+		let ifWon = winnerCheck( player1Name );
+		if ( !ifWon ) {
+			if ( moveCount >= 9 ) {
 				boardMsg("Match Drawn!");
 				moveCount=0;
 				$("#playButton").text("Play again");
-				winnerFound=1;
+				winnerFound = 1;
 				return;
-			}else{
+			} else
+				{
 				turn = player2Name;
-				boardMsg(player2Name+"'s turn now!");
+				boardMsg( player2Name+"'s turn now!" );
 			}
 			return;
 		}
-		else{
+		else {
 			return;
 		}
 
 	}
-	else if(turn==player2Name){
+	else if ( turn == player2Name ) {
 		moveCount++;
 		$(this).text("X");
-		board[row][col] = 2;
-		let ifWon = winnerCheck(2,player2Name);
-		if(!ifWon){
-			if(moveCount>=9){
+		let ifWon = winnerCheck(player2Name);
+		if ( !ifWon ) {
+			if( moveCount >= 9 ){
 				boardMsg("Match Drawn!");
 				moveCount=0;
+				init();   //Added tonight
 				$("#playButton").text("Play again");
 				winnerFound=1;
 				return;
-			}else{
+			} else
+				{
 				turn = player1Name;
 				boardMsg(player1Name+"'s turn now!");
 			}
 			return;
 		}
-		else{
+		else {
 			return;
 		}
 
@@ -123,28 +124,25 @@ $(".col").click(function (){
 
 });
 
-const winnerCheck = function (n,playerName){
+const winnerCheck = function ( playerName ) {
+console.log(`WinnerCheck ${playerName} ${board[0]} ${board[1]} ${board[2]}`);
+	if (
+		 board[0] == playerName && board[1] == playerName && board[2] == playerName ||
+		 board[3] == playerName && board[4] == playerName && board[5] == playerName ||
+		 board[6] == playerName && board[7] == playerName && board[8] == playerName ||
 
-	let winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-	if(
+		 board[0] == playerName && board[3] == playerName && board[6] == playerName ||
+		 board[1] == playerName && board[4] == playerName && board[7] == playerName ||
+		 board[2] == playerName && board[5] == playerName && board[8] == playerName ||
 
+		 board[0] == playerName && board[4] == playerName && board[8] == playerName ||
+		 board[2] == playerName && board[4] == playerName && board[6] == playerName
 
-		(board[0][0]==n && board[0][1]==n && board[0][2]==n) ||
-		(board[1][0]==n && board[1][1]==n && board[1][2]==n) ||
-		(board[2][0]==n && board[2][1]==n && board[2][2]==n) ||
-
-		(board[0][0]==n && board[1][0]==n && board[2][0]==n) ||
-		(board[0][1]==n && board[1][1]==n && board[2][1]==n) ||
-		(board[0][2]==n && board[1][2]==n && board[2][2]==n) ||
-
-		(board[0][0]==n && board[1][1]==n && board[2][2]==n)||
-		(board[0][2]==n && board[1][1]==n && board[2][0]==n)
-
-
-		){
+		 ) {
 		boardMsg(playerName+" won the game!");
 		winnerFound = 1;
-		moveCount=0;
+		moveCount = 0;
+		init();
 		$("#playButton").text("Play again");
 		return true;
 	}

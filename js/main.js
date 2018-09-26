b2813fe7ba379241973abded461d3ae4fa3d913d
John Quach <John.DV.Quach@gmail.com>
2018-09-26 10:15:13 +1000

    Fixed game board not clearing

--- js/main.js
+++ js/main.js
@@ -3,35 +3,36 @@
 //
 
 //Set global variables
-let player1Name = "" , player2Name = "", turn = "";
-let board = [0,0,0,0,0,0,0,0,0];
+let player1Name = "" , player2Name = "", currentPlayer = "";
+let gameBoard = [0,0,0,0,0,0,0,0,0];
 let winnerFound = 0, moveCount=0;
 
 //Function to broadcast message
-const boardMsg = function (x){
+let boardMsg = function (x){
 	return $("#board").text(x);
 }
 
 //Function to randomly choose player 1 or player 2
-const setTurn =function (){
+const setTurn = function () {
 	let randomPlayer = Math.floor((Math.random() * 2) + 1);
 	winnerFound=0;
 	if ( randomPlayer == 1 ){
-		turn = player1Name;
+		currentPlayer = player1Name;
 		boardMsg( player1Name+"'s turn now!" );
 	}
 	else {
-		turn = player2Name;
+		currentPlayer = player2Name;
 		boardMsg( player2Name+"'s turn now!" );
 	}
 }
 //Initialise at start and on win/draw
 const init = function () {
-		turn = "";
-    board = [0,0,0,0,0,0,0,0,0];
+		currentPlayer = "";
+		gameBoard = [0,0,0,0,0,0,0,0,0];
 		boardMsg("");
 		winnerFound = 0;
-		moveCount=0;
+		moveCount = 0;
+		$('.col').empty();
 }
 
 $("#playButton").click(function () {
@@ -44,7 +45,7 @@ $("#playButton").click(function () {
 	player2Name = $("#player2").val();
 
 	if (player1Name == "" || player2Name == ""){
-		alert( "Please set all player names." );
+		alert( "Please set Player 1 and 2 names and press 'Play'." );
 		return;
 	}
 	setTurn();
@@ -56,24 +57,26 @@ $(".col").click(function (){
 	const id = parseInt( $(this).attr('id') );
 	console.log(id);
 
-	board[id] = turn;
-
-	if(player1Name == "" || player2Name == ""){
-		alert("Please set all player names.");
+	if (gameBoard[id]!== 0) {
+		alert("This position is taken. Please try other position.");
 		return;
 	}
-  console.log(board[id]);
-	// if (board[id]!== 0) {
-	// 	alert("This position is taken. Please try other position.");
-	// 	return;
-	// }
+
+// Set the chosen box to the current player
+	gameBoard[id] = currentPlayer;
+	console.log(gameBoard[id]);
+
+	if (player1Name == "" || player2Name == "") {
+		alert("Please set Player 1 and 2 names and press 'Play'.");
+		return;
+	}
 
 	if ( winnerFound == 1 ) {
 		alert( "Please click play again" );
 		return;
 	}
 
-	if ( turn == player1Name ) {
+	if ( currentPlayer == player1Name ) {
 		moveCount++;
 		$(this).text("O");
 
@@ -81,14 +84,16 @@ $(".col").click(function (){
 		if ( !ifWon ) {
 			if ( moveCount >= 9 ) {
 				boardMsg("Match Drawn!");
-				moveCount=0;
-				$("#playButton").text("Play again");
+				moveCount = 0;
+
+				// $("#playButton").text("Play again");
+
 				winnerFound = 1;
 				return;
 			} else
 				{
-				turn = player2Name;
-				boardMsg( player2Name+"'s turn now!" );
+				currentPlayer = player2Name;
+				boardMsg(player2Name+"'s turn now!");
 			}
 			return;
 		}
@@ -97,7 +102,7 @@ $(".col").click(function (){
 		}
 
 	}
-	else if ( turn == player2Name ) {
+	else if ( currentPlayer == player2Name ) {
 		moveCount++;
 		$(this).text("X");
 		let ifWon = winnerCheck(player2Name);
@@ -105,13 +110,11 @@ $(".col").click(function (){
 			if( moveCount >= 9 ){
 				boardMsg("Match Drawn!");
 				moveCount=0;
-				init();   //Added tonight
-				$("#playButton").text("Play again");
+				// $("#playButton").text("Play again");
 				winnerFound=1;
 				return;
-			} else
-				{
-				turn = player1Name;
+			} else {
+				currentPlayer = player1Name;
 				boardMsg(player1Name+"'s turn now!");
 			}
 			return;
@@ -125,25 +128,28 @@ $(".col").click(function (){
 });
 
 const winnerCheck = function ( playerName ) {
-console.log(`WinnerCheck ${playerName} ${board[0]} ${board[1]} ${board[2]}`);
+	console.log(`WinnerCheck ${playerName} ${gameBoard[0]} ${gameBoard[1]} ${gameBoard[2]}`);
+	const g = gameBoard;
 	if (
-		 board[0] == playerName && board[1] == playerName && board[2] == playerName ||
-		 board[3] == playerName && board[4] == playerName && board[5] == playerName ||
-		 board[6] == playerName && board[7] == playerName && board[8] == playerName ||
+		 g[0] === playerName && g[1] === playerName && g[2] === playerName ||
+		 g[3] === playerName && g[4] === playerName && g[5] === playerName ||
+		 g[6] === playerName && g[7] === playerName && g[8] === playerName ||
 
-		 board[0] == playerName && board[3] == playerName && board[6] == playerName ||
-		 board[1] == playerName && board[4] == playerName && board[7] == playerName ||
-		 board[2] == playerName && board[5] == playerName && board[8] == playerName ||
+		 g[0] === playerName && g[3] === playerName && g[6] === playerName ||
+		 g[1] === playerName && g[4] === playerName && g[7] === playerName ||
+		 g[2] === playerName && g[5] === playerName && g[8] === playerName ||
 
-		 board[0] == playerName && board[4] == playerName && board[8] == playerName ||
-		 board[2] == playerName && board[4] == playerName && board[6] == playerName
+		 g[0] === playerName && g[4] === playerName && g[8] === playerName ||
+		 g[2] === playerName && g[4] === playerName && g[6] === playerName
 
 		 ) {
 		boardMsg(playerName+" won the game!");
 		winnerFound = 1;
 		moveCount = 0;
-		init();
+		// debugger
+		console.log('GOT HERE');
 		$("#playButton").text("Play again");
+
 		return true;
 	}
 	return false;
